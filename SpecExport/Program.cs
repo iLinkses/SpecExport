bdc88ec159fe1e010ebf5b9ce9e8f007498d5682
iLinkses <iLinkses@gmail.com>
2022-03-03 15:42:31 +0300

    Наработка загрузки названий чертежей

--- SpecExport/Program.cs
+++ SpecExport/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,16 +25,31 @@ namespace SpecExport
                 log.Trace("MachineName: {0}", Environment.MachineName);
                 log.Trace("UserName: {0}", Environment.UserName);
                 log.Trace("OS: {0}", Environment.OSVersion.ToString());
-                log.Trace("Command: {0}", Environment.CommandLine.ToString());                
+                log.Trace("Command: {0}", Environment.CommandLine.ToString());
 
                 //NLog.Targets.FileTarget tar = (NLog.Targets.FileTarget)LogManager.Configuration.FindTargetByName("filedata");
                 //tar.DeleteOldFileOnStartup = false;
+                GetFileInCatalog();
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Ошибка работы с логом!\n{e.Message}");
             }
             Console.ReadLine();
+        }
+
+        static List<string> FileNames { get; set; } = new List<string>();
+        /// <summary>
+        /// Получает названия файлов с чертежами в каталоге
+        /// </summary>
+        private static void GetFileInCatalog()
+        {
+            string DrawingsDirectory = "Drawings";
+            foreach (var f in Directory.GetFiles(DrawingsDirectory, "*.dwg"))
+            {
+                FileNames.Add(Path.GetFileName(f));
+                Console.WriteLine(Path.GetFileName(f));
+            }
         }
     }
 }
